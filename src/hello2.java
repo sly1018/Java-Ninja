2146dfcbac94fad0627ec25c6768b1eb111dd425
Sulaiman Khawar <s.khawar@chello.at>
2021-02-16 23:26:07 +0100

    with user Input

---
+++ src/hello2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class hello2 {
+	public static void main(String[] args) {
+		Scanner userInputScanner=new Scanner(System.in);
+		System.out.println("What is your name?");
+		String userInputName=userInputScanner.nextLine();
+		System.out.println("Hello "+userInputName+"!");
+	}
+}
