02d3b092214a21910ffaf646f3a827c6786ce027
Sulaiman Khawar <s.khawar@chello.at>
2021-02-17 11:45:29 +0100

    more variable data types

---
+++ src/VariablesAndDatatypes2.java
@@ -0,0 +1,18 @@
+
+public class VariablesAndDatatypes2 {
+	static int count = 0;
+
+	public void increment() {
+		count++;
+	}
+
+	public static void main(String[] args) {
+		VariablesAndDatatypes2 obj1 = new VariablesAndDatatypes2();
+		VariablesAndDatatypes2 obj2 = new VariablesAndDatatypes2();
+		obj1.increment();
+		obj2.increment();
+
+		System.out.println("Obj1: count is = " + obj1.count);
+		System.out.println("Obj2: count is = " + obj2.count);
+	}
+}
