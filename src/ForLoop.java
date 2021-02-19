2108fd8dfb5189feb9e0711d224ef237f8aef905
Sulaiman Khawar <s.khawar@chello.at>
2021-02-19 16:50:27 +0100

    for loop array

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,8 +2,10 @@
 public class ForLoop {
 
 	public static void main(String[] args) {
-		for (int i = 10; i > 1; i--) {
-			System.out.println("Value of i: " + i);
+		int arr[] = { 2, 12, 45, 9 };
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 
