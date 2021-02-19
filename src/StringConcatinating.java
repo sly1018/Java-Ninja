4cb8e861f8b01b6fea108303486e0dc1899d019f
Sulaiman Khawar <s.khawar@chello.at>
2021-02-19 15:00:21 +0100

    java strings

---
+++ src/StringConcatinating.java
@@ -0,0 +1,8 @@
+
+public class StringConcatinating {
+	
+	public static void main(String[] args) {
+		String string1 = "Saw i was";
+		System.out.println("dot " + string1 + " Tod");
+	}
+}
---
+++ src/StringLength.java
@@ -0,0 +1,9 @@
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String palindrome = "dot saw i was tod";
+		int len = palindrome.length();
+		System.out.println("String length is: " + len);
+	}
+}
