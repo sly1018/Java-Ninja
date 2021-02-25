74e1063f5e5009f7b65fd1e8f465d2550e71061c
Sulaiman Khawar <s.khawar@chello.at>
2021-02-25 13:07:09 +0100

    java packages

---
+++ src/mypackage/A.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class A {
+
+	// method
+	public void show() {
+		System.out.println("Some method"); // console output
+	}
+
+}
---
+++ src/mypackage/Hello.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Hello {
+
+	// main
+	public static void main(String[] args) {
+		A a = new A(); // creating object
+		a.show(); // calling show method
+		System.out.println("Show() class A"); // console output
+	}
+
+}
